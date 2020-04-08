c2532ecc4603ce5b7429f8498022a281e0cfa827
ATechnoHazard <amolele@gmail.com>
2020-04-08 18:21:51 +0530

    components: Finish the Home component
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

---
+++ src/components/Home.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import { Jumbotron, Button, Container, Row, Col, Card } from "react-bootstrap";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Jumbotron fluid>
+          <Container>
+            <Row>
+              <Col lg={true}>
+                <h1>Welcome to the Mercury DEX!</h1>
+                <p>
+                  We allow trading of NFTs for TEST tokens using atomic
+                  Plasma-backed swaps.
+                </p>
+
+                <p>
+                  <Button variant="primary">Learn more</Button>
+                </p>
+              </Col>
+              <Col lg={true}>
+                <img src="" alt=""></img>
+              </Col>
+            </Row>
+          </Container>
+        </Jumbotron>
+
+        <br></br>
+
+        <Container>
+          <Row>
+            <Col lg={true} className="my-auto">
+              <Card className="border-0">
+                <Card.Body>
+                  <Card.Title>What are swaps?</Card.Title>
+                  <Card.Text>
+                    Swaps are a financial instrument, which in the context of a
+                    decentralized exchange (DEX) mean a way to safely exchange
+                    Non-Fungible Tokens (NFTs) for regular tokens.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col lg={true}>
+              <img
+                src="https://boxswap.io/static/media/circle-animated.5926fac6.svg"
+                alt=""
+              ></img>
+            </Col>
+          </Row>
+
+          <Row style={{ marginTop: "100px", backgroundColor: "#f1f3f6" }}>
+            <Col lg={true}>
+              <img
+                src="https://matic.network/_nuxt/c29fd82f84b3eb89fd5748d22eaa05cc.svg"
+                alt=""
+              ></img>
+            </Col>
+
+            <Col lg={true} className="my-auto">
+              <Card className="border-0" style={{ backgroundColor: "#f1f3f6" }}>
+                <Card.Body>
+                  <Card.Title>How swaps work</Card.Title>
+                  <Card.Text>
+                    Swaps on our DEX take place through a Plasma smart contract,
+                    deployed on the Matic Blockchain.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+
+        <div>
+          <Container>
+            <Row style={{ marginTop: "100px" }}>
+              <Col lg={true}>
+                <Card className="border-0">
+                  <Card.Body>
+                    <Card.Title>How swaps work</Card.Title>
+                    <Card.Text>
+                      Swaps on our DEX take place through a Plasma smart
+                      contract, deployed on the Matic Blockchain.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col lg={true}></Col>
+              <Col lg={true}></Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
