e9ba61cc10881a25489834180690568f9bd8b223
ATechnoHazard <amolele@gmail.com>
2020-04-08 22:48:08 +0530

    treewide: Improve UI and general UX
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import { Navbar, Nav } from "react-bootstrap";
 import Trade from "./components/Trade";
 
@@ -9,13 +10,36 @@ export default function App() {
   return (
     <Router>
       <div>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="/">Mercury</Navbar.Brand>
+        <Navbar
+          id="mainNav"
+          className="fixed-top"
+          expand="lg"
+          style={{ backgroundColor: "#0a1647", padding: "20px" }}
+        >
+          <Navbar.Brand
+            style={{
+              color: "white",
+              marginLeft: "50px",
+              fontFamily: "Lato",
+              fontSize: "34px",
+              fontWeight: "bold",
+            }}
+            href="/"
+          >
+            Mercury
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
           <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/trade">Create Order</Nav.Link>
+            <Nav.Link href="/" style={{ color: "white", fontFamily: "Lato" }}>
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="/trade"
+              style={{ color: "white", fontFamily: "Lato" }}
+            >
+              Create Order
+            </Nav.Link>
           </Nav>
         </Navbar>
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,25 +2,36 @@ import React, { Component } from "react";
 import { Jumbotron, Button, Container, Row, Col, Card } from "react-bootstrap";
 
 class Home extends Component {
+  scrollBtn = (event) => {
+    event.preventDefault();
+    window.scrollBy(0, window.screen.height);
+  };
+
   render() {
     return (
-      <div>
-        <Jumbotron fluid>
+      <div
+        style={{ paddingTop: "100px" }}
+      >
+        <Jumbotron
+          fluid
+          style={{ backgroundColor: "#0a1647", height: window.screen.height }}
+        >
           <Container>
             <Row>
               <Col lg={true}>
-                <h1>Welcome to the Mercury DEX!</h1>
-                <p>
+                <h1 style={{ color: "white", fontFamily: "Lato" }}>
+                  Welcome to the Mercury DEX!
+                </h1>
+                <p style={{ color: "white", fontFamily: "Roboto" }}>
                   We allow trading of NFTs for TEST tokens using atomic
                   Plasma-backed swaps.
                 </p>
 
                 <p>
-                  <Button variant="primary">Learn more</Button>
+                  <Button onClick={this.scrollBtn} variant="primary">
+                    Learn more
+                  </Button>
                 </p>
-              </Col>
-              <Col lg={true}>
-                <img src="" alt=""></img>
               </Col>
             </Row>
           </Container>
@@ -28,16 +39,44 @@ class Home extends Component {
 
         <br></br>
 
-        <Container fluid style={{textAlign: "center"}}>
+        <Container fluid style={{ textAlign: "center" }}>
           <Row>
             <Col lg={true} className="my-auto">
-              <Card className="border-0" style={{textAlign: "left"}}>
+              <Card
+                className="border-0"
+                style={{
+                  textAlign: "left",
+                  paddingLeft: "200px",
+                  paddingRight: "50px",
+                }}
+              >
                 <Card.Body>
-                  <Card.Title>What are swaps?</Card.Title>
-                  <Card.Text>
-                    Swaps are a financial instrument, which in the context of a
-                    decentralized exchange (DEX) mean a way to safely exchange
-                    Non-Fungible Tokens (NFTs) for regular tokens.
+                  <Card.Title>
+                    <h2
+                      style={{
+                        fontWeight: "700",
+                        fontSize: "34px",
+                        fontFamily: "Lato, sans-serif",
+                        color: "#0a1647",
+                        lineHeight: "1.5",
+                      }}
+                    >
+                      What are swaps?
+                    </h2>
+                  </Card.Title>
+                  <Card.Text style={{ fontFamily: "Roboto, sans-serif" }}>
+                    <p>
+                      Swaps are a financial instrument, which in the context of
+                      a decentralized exchange (DEX) mean a way to safely
+                      exchange Non-Fungible Tokens (NFTs) for regular tokens.
+                    </p>
+
+                    <p>
+                      This might be useful in a situation like say, a game in
+                      which NFTs represent items in the game, which can be
+                      purchased using the in-game currency, represented by
+                      regular tokens.
+                    </p>
                   </Card.Text>
                 </Card.Body>
               </Card>
@@ -51,7 +90,14 @@ class Home extends Component {
             </Col>
           </Row>
 
-          <Row style={{ marginTop: "100px", backgroundColor: "#f1f3f6", padding: "40px" }}>
+          <Row
+            style={{
+              marginTop: "100px",
+              backgroundColor: "#f1f3f6",
+              paddingTop: "100px",
+              paddingBottom: "100px",
+            }}
+          >
             <Col lg={true}>
               <img
                 src="https://matic.network/_nuxt/c29fd82f84b3eb89fd5748d22eaa05cc.svg"
@@ -60,12 +106,47 @@ class Home extends Component {
             </Col>
 
             <Col lg={true} className="my-auto">
-              <Card className="border-0" style={{ backgroundColor: "#f1f3f6",textAlign: "left" }}>
+              <Card
+                className="border-0"
+                style={{
+                  backgroundColor: "#f1f3f6",
+                  textAlign: "left",
+                  paddingRight: "110px",
+                }}
+              >
                 <Card.Body>
-                  <Card.Title><h4 style={{fontWeight: "bold"}}>How swaps work</h4></Card.Title>
-                  <Card.Text>
-                    Swaps on our DEX take place through a Plasma smart contract,
-                    deployed on the Matic Blockchain.
+                  <Card.Title>
+                    <h2
+                      style={{
+                        fontWeight: "700",
+                        fontSize: "34px",
+                        fontFamily: "Lato, sans-serif",
+                        color: "#0a1647",
+                        lineHeight: "1.5",
+                      }}
+                    >
+                      How swaps work
+                    </h2>
+                  </Card.Title>
+                  <Card.Text style={{ fontFamily: "Roboto, sans-serif" }}>
+                    <p>
+                      Swaps on our DEX take place through a Plasma smart
+                      contract, deployed on the Matic Blockchain. The Plasma
+                      smart contract takes a signed order from an account, and
+                      upon matching of that order with the owner of the NFT, the
+                      tokens are atomically swapped for each other.
+                    </p>
+
+                    <p>
+                      Our swaps take place on the Matic network. This provides a
+                      solution for faster and extremely low-cost transactions
+                      with finality on the main chain. The system ensures
+                      liveliness using PoS checkpoints which are pushed to the
+                      Ethereum mainchain. This enables a single Matic sidechain
+                      to theoretically achieve 2<sup>16</sup> transactions per
+                      block, and possibly millions of transactions on multiple
+                      chains in the future.
+                    </p>
                   </Card.Text>
                 </Card.Body>
               </Card>
